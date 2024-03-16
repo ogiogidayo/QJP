67c12a8b11a48514678dbc5a4e4054fa5ff7b2f4
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-16 17:56:12 +0900

    feat

--- src/main/java/projava/Circles.java
+++ src/main/java/projava/Circles.java
@@ -1,6 +1,7 @@
 package projava;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Circles {
@@ -9,6 +10,11 @@ public class Circles {
         var g = image.createGraphics();
         for (int x = 0; x < 12; x++) {
             for (int y = 0; y < 12; y++) {
+                if (x == 3) {
+                    g.setColor(Color.RED);
+                } else {
+                    g.setColor(Color.white);
+                }
                 g.fillOval(x * 30 + 50, y * 30 + 20, 25, 25);
             }
         }
