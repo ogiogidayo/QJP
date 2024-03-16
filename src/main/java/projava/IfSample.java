1e1a1baec04ae0440962f83ec142bee63dff28e5
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-16 16:32:37 +0900

    init commmit

---
+++ src/main/java/projava/IfSample.java
@@ -0,0 +1,10 @@
+package projava;
+
+public class IfSample {
+    public static void main(String[] args) {
+        var a = 2;
+        if (a < 3) {
+            System.out.println("Small");
+        }
+    }
+}
