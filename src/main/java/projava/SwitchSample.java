7f41499743129e8c2d8e9002a5ace35fce4d5953
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-16 16:37:30 +0900

    feat

--- src/main/java/projava/SwitchSample.java
+++ src/main/java/projava/SwitchSample.java
@@ -3,12 +3,10 @@ package projava;
 public class SwitchSample {
     public static void main(String[] args) {
         var a = 3;
-        if (a == 1 || a == 2) {
-            System.out.println("one-two");
-        } else if (a == 3) {
-            System.out.println("three");
-        } else {
-            System.out.println("four");
+        switch (a) {
+            case 1, 2 -> System.out.println("one-two");
+            case 3 -> System.out.println("three");
+            default -> System.out.println("four");
         }
     }
 }
