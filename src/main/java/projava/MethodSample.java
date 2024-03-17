936ade49d3eb4bed6174fd8df3c502a02798db8c
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-17 11:03:28 +0900

    feat

--- src/main/java/projava/MethodSample.java
+++ src/main/java/projava/MethodSample.java
@@ -3,8 +3,9 @@ package projava;
 public class MethodSample {
     public static void main(String[] args) {
         var result = twice(3);
-        var result2 = MethodSample.twice(3); // 同じ
+        var result2 = MethodSample.twice(4); // 同じ
         System.out.println(result);
+        System.out.println(result2);
     }
 
     static int twice(int x) {
