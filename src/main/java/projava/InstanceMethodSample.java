4ee21cd4be65fe7025a53ca73b4fb4b5b7e0dc0f
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-17 11:07:56 +0900

    feat

--- src/main/java/projava/InstanceMethodSample.java
+++ src/main/java/projava/InstanceMethodSample.java
@@ -5,11 +5,17 @@ public class InstanceMethodSample {
         int average() {
             return (this.englishScore() + this.mathScore()) / 2;
         }
+
+        public int maxScore() {
+            return Math.max(englishScore(), mathScore());
+        }
     }
 
     public static void main(String[] args) {
         var kis = new Student("kis", 60, 80);
         var a = kis.average();
+        int max = kis.maxScore();
         System.out.println("平均点は%d点です".formatted(a));
+        System.out.println("最高点は%d点です".formatted(max));
     }
 }
